c91969442fc0f3a01163d947bece843dc68f19bf
Yilmaz Mustafa <dev@mail.be>
2025-04-25 15:53:25 +0200

    modern build tool is chosen.

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Clean previous builds
-rm -rf dist/ build/ *.egg-info/
-pip install wheel setuptools python-dotenv numpy scipy torch torchaudio pyannote.audio librosa soundfile matplotlib PyAudio numba onnxruntime
-pip install --upgrade transformers 'datasets[audio]' accelerate
-
-pip install .
